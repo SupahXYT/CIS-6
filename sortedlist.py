fac2d7505cf9919695ee5c50f0d5b13d91281547
supahxyt <josuefirelol@gmail.com>
2021-09-02 22:37:28 -0700

    finished

--- sortedlist.py
+++ sortedlist.py
@@ -9,13 +9,26 @@ class sortedList():
             random = randrange(0, len(self._list))
             self._list.insert(i, self._list.pop(random))
                                                                                                                         
-   def mergeSort(self):                                                                                                   
-       max = int(len())
+    def mergeSort(self):
+        self._list = self._split(self._list)
 
-    def _merge(self, left, right): # Returns merged list
+        while(len(self._list) > 1):
+            r = []; i = 0
+
+            while(i < len(self._list)):
+                left = self._list[i:i+1]; 
+                right = self._list[i+1:i+2]
+
+                r.append(self.merge(left[0], right[0] if(len(right) > 0) else []))
+                i += 2
+            self._list = r
+        
+        self._list = [] if(len(self._list) == 0) else self._list[0]
+
+    def merge(self, left, right): 
         merge = []
 
-        while(len(left) > 0 or len(right) > 0): # While there exists elements in either list
+        while(len(left) > 0 or len(right) > 0): 
             while(len(left) > 0):
                 if(len(right) > 0 and left[0] < right[0]):
                     merge.append(left.pop(0))
@@ -36,18 +49,18 @@ class sortedList():
     def _split(self, list):
         s = []
         for i in range(0, len(list)):
-            a = []
-            a.append(list[i])
+            a = [list[i]]
             s.append(a)
         return s
 
     def getList(self):                                                                                                  
         return self._list                                                                                               
                                                                                                                         
-a = sortedList([1, 2, 3, 4, 5, 6])                                                                                                      
-                                                                                                                        
-print(a.getList())                                                                                                      
-a.shuffle()                                                                                                             
-print(a.getList())                    
-print("main:" + str(a._merge([11230], [9, 11, 99, 100, 235])))
+a = sortedList([1])                                                                                                      
+print(f'original: {a.getList()}')                                                                                                      
 
+a.shuffle()                                                                                                             
+print(f'shuffle: {a.getList()}')                    
+
+a.mergeSort()
+print(f'sorted: {a.getList()}')                    
