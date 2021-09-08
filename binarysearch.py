c781619fc98cb48220fa127f48a89500d285177f
SupahX <59423340+SupahXYT@users.noreply.github.com>
2021-09-08 08:35:49 +0000

    Update binarysearch.py

--- binarysearch.py
+++ binarysearch.py
@@ -1,41 +1,20 @@
-
+    
 def binarysearch(a, n):
     pivot = int(len(a)/2)
 
-    print(a)
-    print(pivot)
-
-    if(a[pivot] == n):
+    if(len(a) < 1):
+        return None
+    elif(a[pivot] == n):
         return pivot
     elif(a[pivot] > n):
         return binarysearch(a[:pivot], n)
     else:
-        return binarysearch(a[pivot:], n)
+        s = binarysearch(a[pivot+1:], n)
+        return (s + pivot + 1) if s != None else None
 
-def reverse(a):
-    for i in range(len(a), 0):
-        print(a)
+a = [1, 4, 7, 8, 12, 31]
 
-# List implemenation of binary heap
-# reverse sorted list fufils the heap condition
-def _binarysearch(a, n):
-    heap = list.reverse(a); i = 0
-    print(heap)
-
-    while(i < len(heap)):
-        leftchild = (i*2)+1
-        rightchild = (i*2)+2
-        pivot = int(len(a)/2)
-
-        if(heap[pivot] == n):
-            return heap[pivot]
-        elif(heap[leftchild] > heap[pivot]):
-            i = leftchild
-        elif(heap[rightchild] < heap[pivot]):
-            i = rightchild
-
-    return None
-
-a = [1, 4, 7, 8, 12, 31, 100]
-reverse(a)
+i = 7
+print(a)
+print(f'{i} in index: {binarysearch(a, i)}')
 
